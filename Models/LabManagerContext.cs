a8647570ed9372c2a9ae8021fcc38e86cdc518b7
Nicole Miguel <nicolemonteiromiguel@gmail.com>
2022-11-03 16:03:59 -0300

    first commit

---
+++ Models/LabManagerContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcLabManager.Models;
+
+public class LabManagerContext : DbContext
+{
+    public DbSet<Computer> Computers {get; set; }
+    //construtor recebe as opções de configuração
+    public LabManagerContext(DbContextOptions<LabManagerContext> options) : base(options)
+    {
+
+    }
+}
