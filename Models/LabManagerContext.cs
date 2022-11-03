2e9976ddcc870bac35d2f9942ef175d3f82a4aa7
Nicole Miguel <nicolemonteiromiguel@gmail.com>
2022-11-03 16:45:40 -0300

    crud lab

--- Models/LabManagerContext.cs
+++ Models/LabManagerContext.cs
@@ -5,6 +5,7 @@ namespace MvcLabManager.Models;
 public class LabManagerContext : DbContext
 {
     public DbSet<Computer> Computers {get; set; }
+    public DbSet<Lab> Labs {get; set; }
     //construtor recebe as opções de configuração
     public LabManagerContext(DbContextOptions<LabManagerContext> options) : base(options)
     {
